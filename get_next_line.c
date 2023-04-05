33766bd2e223d706a1427989bee27b4e32222c15
ldufour <leonpierre.dufour@gmail.com>
2023-04-04 20:36:55 -0400

    Doit nettoyer stash

--- get_next_line.c
+++ get_next_line.c
@@ -6,44 +6,61 @@
 /*   By: leon <leon@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 09:49:41 by leon              #+#    #+#             */
-/*   Updated: 2023/04/04 16:11:29 by leon             ###   ########.fr       */
+/*   Updated: 2023/04/04 20:36:31 by leon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-// #include "get_next_line_utils.c"
+#include "get_next_line_utils.c"
 
-char *extract_line(char *str) {
-  char *line;
-  int i;
+char	*extract_line(char *str)
+{
+	char	*line;
+	int		i;
+	int		len;
 
-  i = 0;
-  if (!str)
-    return (NULL);
-  line = malloc(sizeof(char) * 10);
-  while (str[i] != '\n' || str[i]) {
-    line[i] = str[i];
-    i++;
-  }
-  line[i] = '\0';
-  return (str);
+	i = 0;
+	len = 0;
+	if (!str)
+		return (NULL);
+	while (str[len] && str[len] != '\n')
+		len++;
+	line = malloc(sizeof(char) * (len + i));
+	if (!line)
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+	{
+		line[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+	{
+		line[i] = str[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (str);
 }
 
-char *get_next_line(int fd) {
-  char *line;
-  char *stash;
-  char *buffer;
-  int read_bytes;
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*stash;
+	char		*buffer;
+	int			read_bytes;
 
-  read_bytes = 1;
-  stash = ft_strdup("");
-  buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-  while (!ft_strchr(stash, '\n') && read_bytes != 0) {
-    read_bytes = read(fd, buffer, BUFFER_SIZE);
-    buffer[read_bytes] = '\0';
-    stash = ft_strjoin(stash, buffer);
-  }
-  line = extract_line(stash);
-  free(buffer);
-  return (line);
+	if (fd < 0 || fd > 1023 || BUFFER_SIZE < 0)
+		return (NULL);
+	read_bytes = 1;
+	stash = ft_strdup("");
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while (!ft_strchr(stash, '\n') && read_bytes != 0)
+	{
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		buffer[read_bytes] = '\0';
+		stash = ft_strjoin(stash, buffer);
+	}
+	line = extract_line(stash);
+	free(buffer);
+	return (line);
 }
--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include "get_next_line.c"
 #include "get_next_line.h"
 
-int main() {
-  int fd;
-  char *line;
+int	main(void)
+{
+	int		fd;
+	char	*line;
 
-  fd = open("test.txt", O_RDONLY);
-  while (line != NULL) {
-    line = get_next_line(fd);
-    printf("%s", line);
-  }
-  fd = close(fd);
-  return (0);
+	fd = open("test.txt", O_RDONLY);
+	while (line != NULL)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+	}
+	fd = close(fd);
+	return (0);
 }
